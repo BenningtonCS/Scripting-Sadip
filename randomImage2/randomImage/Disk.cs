9bbf51ba5a3e5b63170751bcb88457fb11e30b40
Sadip Giri <sadipgiri@bennington.edu>
2016-03-19 13:01:34 -0700

    addition of disk class
    checking whether the ray intersects the disk or not through the use of
    intersection approach of plane containing that disk.

---
+++ randomImage2/randomImage/Disk.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace randomImage
+{
+    class Disk
+    {
+        public Vector normal;
+        public Vector center;
+        public double radius;
+        public SColor color;
+
+        public Disk(Vector normal, Vector center, double radius, SColor color) {
+            this.normal = normal;
+            this.center = center;
+            this.radius = radius;
+            this.color = color;
+        }
+
+        // knowing whether the ray intersect the disk or not 
+        public bool DoesIntersect(Vector origin, Vector direction) { 
+            double t = 0;
+            if (Plane.DoesIntersect(origin, direction)) // at first let's find whether the ray intersect the plane or not 
+            {                                           // if so then 
+                Vector p = origin + direction * t;      // point of intersection in the plane
+                Vector q = p - center;                  // position vector from point to center of the disk
+                double d = Vector.magnitude(q);         // magnitude of the vector which gives the distance inside the disk
+                return (d <= radius);                   // then that distance should be smaller than that of radius of the disk according to geometric approach.
+            }
+
+            return false;
+        }
+    }
+}
--- randomImage2/randomImage/Plane.cs
+++ randomImage2/randomImage/Plane.cs
@@ -10,17 +10,17 @@ namespace randomImage
     {
         public Vector normal; // this is the normal vector of the plane
         public Vector point; // this is a point on a plane which is also a Vector coordinate
-        public SColor planeColor; // this is the color assigned to plane
+        public SColor color; // this is the color assigned to plane
 
         // plane is defined here
         public Plane(Vector normal, Vector point, SColor planeColor) { 
             this.normal = normal;
             this.point = point;
-            this.planeColor = planeColor;
+            this.color = planeColor;
         }
 
         // does the ray intersect the plane?
-        public bool DoesIntersect(Vector origin, Vector direction) {
+        public static bool DoesIntersect(Vector origin, Vector direction) {
             double t = 0; // setting the value of t as zero first
             double denominator = normal * direction; // from the formula t = ((point - Origin)*normal)/(direction * normal)
             if (denominator > 0) {
